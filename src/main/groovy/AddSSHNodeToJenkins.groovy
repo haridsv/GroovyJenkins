ac7f94f455a712bf16451a2dafcd14e0601fc72e
Hari Krishna Dara <haridara@gmail.com>
2018-07-06 17:39:44 +0530

    Updated the API since the original API is now deprecated
    The constructors from DumbSlave and SSHLauncher are deprecated, so updated them.

--- src/main/groovy/AddSSHNodeToJenkins.groovy
+++ src/main/groovy/AddSSHNodeToJenkins.groovy
@@ -48,13 +48,13 @@ if (agentName.contains("engine")) {
 
 // There is a constructor that also takes a list of properties (env vars) at the end, but haven't needed that yet
 DumbSlave dumb = new DumbSlave(agentName, // Agent name, usually matches the host computer's machine name
-        agentDescription, // Agent description
         agentHome, // Workspace on the agent's computer
-        agentExecutors, // Number of executors
-        Mode.EXCLUSIVE, // "Usage" field, EXCLUSIVE is "only tied to node", NORMAL is "any"
-        agentLabels, // Labels
-        new SSHLauncher(agentIP, 22, SSHLauncher.lookupSystemCredentials(credentialID), "", null, null, "", "", 60, 3, 15),
-        RetentionStrategy.INSTANCE) // Is the "Availability" field and INSTANCE means "Always"
+        new SSHLauncher(agentIP, 22, credentialID, "", null, "", "", 60, 3, 15))
+dumb.setNodeDescription(agentDescription) // Agent description
+dumb.setNumExecutors(agentExecutors) // Number of executors
+dumb.setMode(Mode.EXCLUSIVE) // "Usage" field, EXCLUSIVE is "only tied to node", NORMAL is "any"
+dumb.setLabelString(agentLabels) // Labels
+dumb.setRetentionStrategy(RetentionStrategy.INSTANCE) // Is the "Availability" field and INSTANCE means "Always"
 Jenkins.instance.addNode(dumb)
 println "Agent '$agentName' created with $agentExecutors executors and home '$agentHome'"
 
